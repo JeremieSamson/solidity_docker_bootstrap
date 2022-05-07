006ef91062d7a8bba97450330d434a5bedafd0cc
Jérémie Samson <jeremie.samson76@gmail.com>
2022-05-07 15:54:20 +0200

    Solium fix

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.4.21 <8.10.0;
 
 contract SimpleStorage {
-  uint storedData;
+    uint storedData;
 
-  function set(uint x) public {
-    storedData = x;
-  }
+    function set(uint x) public {
+        storedData = x;
+    }
 
-  function get() public view returns (uint) {
-    return storedData;
-  }
+    function get() public view returns (uint) {
+        return storedData;
+    }
 }
